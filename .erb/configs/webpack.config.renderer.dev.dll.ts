b9847c541f8678b860d8a06f331120fb2a79f608
Remie <6835891+Nantris@users.noreply.github.com>
2025-03-18 18:27:43 -0400

    Repro for electron-builder #8970

--- .erb/configs/webpack.config.renderer.dev.dll.ts
+++ .erb/configs/webpack.config.renderer.dev.dll.ts
@@ -23,7 +23,7 @@ const configuration: webpack.Configuration = {
 
   target: 'electron-renderer',
 
-  externals: ['fsevents', 'crypto-browserify'],
+  externals: ['sharp', 'fsevents', 'crypto-browserify'],
 
   /**
    * Use `module` from `webpack.config.renderer.dev.js`
